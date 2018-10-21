4f587d02e0e60ee3105720bb6d42a24059d5808b
League <league@WTS20.attlocal.net>
2018-10-21 15:59:44 -0700

    textured tri

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,3 +1,5 @@
+uniform float redColor;
+
 void main(){
-    gl_FragColor = vec4(0, 0, 1, 1);
+    gl_FragColor = vec4(redColor, 0, 1, 1);
 }
--- main.cpp
+++ main.cpp
@@ -8,20 +8,21 @@ int main(int argc, char** argv){
     SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 900, 500, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    long vertFileSize, fragFileSize;
     char* vertexData = readFileToCharArray("vertex_shader.glsl");
     char* fragData = readFileToCharArray("fragment_shader.glsl");
 
     unsigned int shader = compileShaderVF(vertexData, fragData);
     glUseProgram(shader);
     int postionId = glGetAttribLocation(shader, "position");
+    int texCoordId = glGetAttribLocation(shader, "texCoord");
+    int redColorId = glGetUniformLocation(shader, "redColor");
+    
 
     float verts[] = {
-        -0.8, -0.8,
-        0.0, 0.8,
-        0.8, -0.8,
+        -0.8, -0.8, 0, 1, 
+        0.0, 0.8, 0.5, 0,
+        0.8, -0.8, 1, 1,
     };
-
 
     unsigned int vao, vbo;
     glGenVertexArrays(1, &vao);
@@ -30,11 +31,27 @@ int main(int argc, char** argv){
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(postionId, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glVertexAttribPointer(postionId, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
     glEnableVertexAttribArray(postionId);
+    glVertexAttribPointer(texCoordId, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(texCoordId);
+
+    unsigned char texture[] = {
+        255,255,255,255,0,0,0,255,
+        0,0,0,255,255,255,255,255
+    };
+    unsigned int tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture); 
 
     glClearColor(1, 1, 0, 1);
 
+    float rC = 0.0;
     SDL_Event event;
     bool isRunning = true;
     while(isRunning){
@@ -53,7 +70,11 @@ int main(int argc, char** argv){
             }
         }
         glClear(GL_COLOR_BUFFER_BIT);
-
+        rC += 0.01;
+        if(rC > 1){
+            rC = 0;
+        }
+        glUniform1f(redColorId, rC);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         SDL_GL_SwapWindow(window);
